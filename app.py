6cdb31f86b5332d11339b5d7baeaffd6723385d6
YetAnother_yk <www.yklab.net@gmail.com>
2016-12-19 17:15:52 +0900

    update app.py & rm class view

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import os
 from bottle import route, run
+from bottle import TEMPLATE_PATH, jinja2_template as template
 
+# index.pyが設置されているディレクトリの絶対パスを取得
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# テンプレートファイルを設置するディレクトリのパスを指定
+TEMPLATE_PATH.append(BASE_DIR + "/templates")
+
+@route('/')
+def index():
+    return template('index.tpl.html')
 
 @route('/hello')
 def hello():
