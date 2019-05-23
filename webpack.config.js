b8286dd890350b67bd0003e3b3ec5ef9639fc15e
y_k <yetanother.yk@gmail.com>
2019-05-23 11:00:35 +0900

    #20

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: "development",
+  mode: "production",
   entry: {
     main: "./static/src/main.ts",
     stop_times: "./static/src/stop_times.ts",
